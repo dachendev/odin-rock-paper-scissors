d5c27b77992e42861ea4db9cf0adf147b475d08f
Jacob Dachenhaus <dachendev@gmail.com>
2023-08-06 17:50:00 -0400

    Add pseudocode

---
+++ game.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    // randomly return rock, paper, or scissors
+}
+
+function playRound(playerSelection, comptuerSelection) {
+    // make sure inputs are case-insensitive
+
+    // return a string that declares the winner of the round
+    // eg. "You Lose! Paper beats Rock"
+}
+
+function game() {
+    // call playRound() 5 times
+    // use prompt to take input from the player
+    // use console.log to display the results, and the overall winner
+}
